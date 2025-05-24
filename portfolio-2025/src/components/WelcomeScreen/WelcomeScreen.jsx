2f61f439c97fb3def9888b89ac8711d5db7f009a
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-05-24 05:19:11 -0300

    24/05: Corrección de error en botones

--- portfolio-2025/src/components/WelcomeScreen/WelcomeScreen.jsx
+++ portfolio-2025/src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -152,7 +152,7 @@ const WelcomeScreen = () => {
         >
           <FaMoon className="text-[0.1rem] sm:text-[1rem] md:text-[1rem]" />
           <span
-            className={`ml-1 sm:ml-2 text-xs text-[8px] tracking-[3px]  font-hacker${
+            className={`ml-1 sm:ml-2 text-[8px] tracking-[3px]  font-hacker ${
               glitchActive ? "glitch-effect-small" : ""
             }`}
           >
