d58db5c074dd266a29273b7ada37bf29ffe1d032
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-07-11 01:31:24 -0300

    11/07: Implementación de efectos y diseño responsive a ModuleGrid.jsx

--- portfolio-2025/src/components/SystemLoader/Modules/ModuleGrid.jsx
+++ portfolio-2025/src/components/SystemLoader/Modules/ModuleGrid.jsx
@@ -9,10 +9,9 @@ const ModuleGrid = ({ modules, activeModules }) => {
   }, []);
 
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-6 gap-2 mb-4 w-full max-w-xl mx-auto mt-8">
+    <div className="grid grid-cols-6 sm:grid-cols-6 md:grid-cols-6 gap-2 mb-4 sm:gap-3 md:gap-4 w-full max-w-xl sm:max-w-xl mx-auto mt-12 sm:mt-8">
       {modules.map((module, i) => {
         const isActive = activeModules.includes(i);
-
         useEffect(() => {
           if (isActive && !activeAnimations.includes(i)) {
             const timer = setTimeout(() => {
@@ -21,23 +20,28 @@ const ModuleGrid = ({ modules, activeModules }) => {
             return () => clearTimeout(timer);
           }
         }, [isActive, i, activeAnimations]);
-
         return (
           <motion.div
             key={i}
-            className={`module-${i} flex flex-col items-center p-2 transition-colors duration-[3000ms] ease-[cubic-bezier(0.4,0,0.2,1)] ${
-              activeAnimations.includes(i)
-                ? `${module.color} `
-                : "text-gray-500"
+            className={`module-${i} flex flex-col items-center p-1 sm:p-2 transition-colors duration-[3000ms] ease-[cubic-bezier(0.4,0,0.2,1)] ${
+              activeAnimations.includes(i) ? module.color : "text-gray-500"
             }`}
-            initial={{ opacity: 0, y: 5 }}
-            animate={{ opacity: 1, y: isActive ? -5 : 0 }}
-            transition={{ delay: i * 0.1, type: "spring", stiffness: 100 }}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: isActive ? -10 : 0 }}
+            transition={{
+              duration: 1.2,
+              ease: "easeOut",
+              delay: i * 0.1,
+              type: "spring",
+              stiffness: 100,
+            }}
           >
-            <span className={`module-icon module-icon-${i} text-xl mb-1`}>
+            <span
+              className={`module-icon module-icon-${i} text-lg sm:text-xl md:text-xl xl-text-xl mb-1`}
+            >
               {module.icon}
             </span>
-            <span className="text-[8px] font-montserrat text-center">
+            <span className="hidden sm:inline text-[7px] sm:text-[8px] md:text-[10px] xl:text-[8px] font-montserrat text-center leading-tight">
               {module.label}
             </span>
           </motion.div>
