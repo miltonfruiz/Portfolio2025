6959b0ff656c5821bf730a66b90374d00d605f01
Milton Franco Ruiz <miltonfruizok@outlook.es>
2025-07-10 22:09:17 -0300

    10/07: Implementación de fectos y diseño responsive a SystemHeader.jsx

--- portfolio-2025/src/components/SystemLoader/Modules/SystemHeader.jsx
+++ portfolio-2025/src/components/SystemLoader/Modules/SystemHeader.jsx
@@ -1,32 +1,60 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { FaServer } from "react-icons/fa";
 
 const SystemHeader = ({ finalStage, message, progress }) => {
   return (
-    <div className="flex items-center mb-4 mt-6 space-x-4 w-full max-w-xl mx-auto px-4">
-      <FaServer
-        className={`text-3xl transition-colors duration-7000 ease-[cubic-bezier(0.4,0,0.2,1)] ${
-          progress > 99.9
-            ? "text-cyber-primary animate-pulse-fast"
-            : "text-[#555555] animate-pulse shadow-[#555555]"
-        }`}
-      />
-      <div>
-        <h1 className="text-xs font-bold text-cyber-primary tracking-wider font-hacker">
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={{ visible: { transition: { staggerChildren: 0.2 } } }}
+      className="flex items-center mb-4 mt-6 space-x-4 w-full max-w-[90vw] sm:max-w-xl md:max-w-2xl xl:max-w-xl mx-auto px-4"
+    >
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: -20 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        transition={{ duration: 1.2, ease: "easeOut" }}
+      >
+        <FaServer
+          className={`text-2xl sm:text-3xl md:text-4xl xl:text-3xl transition-colors duration-7000 ease-[cubic-bezier(0.4,0,0.2,1)] ${
+            progress > 99.9
+              ? "text-cyber-primary animate-pulse-fast"
+              : "text-[#555555] animate-pulse shadow-[#555555]"
+          }`}
+        />
+      </motion.div>
+      <div className="space-y-1">
+        <motion.h1
+          variants={{
+            hidden: { opacity: 0, x: 20 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          transition={{ duration: 1.2, ease: "easeOut" }}
+          className={`text-[10px] sm:text-xs md:text-sm xl:text-[11px] font-bold tracking-wider font-hacker transition-colors duration-700 ${
+            finalStage ? "text-cyber-primary" : "text-[#555]"
+          }`}
+        >
           CYBER_SYSTEM v4.2.5
-        </h1>
-        <p
+        </motion.h1>
+        <motion.p
           id="system-message"
-          className={`text-[10px] ${
+          variants={{
+            hidden: { opacity: 0, x: -20 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          transition={{ duration: 1.2, ease: "easeOut" }}
+          className={`text-[8px] sm:text-[10px] md:text-xs xl:text-[9.5px] ${
             finalStage
               ? "text-green-700 font-bold tracking-wider"
               : "text-cyber-secondary"
           } font-mono mt-2`}
         >
           {message}
-        </p>
+        </motion.p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
